4686258e8bdc7667ec89411f3c462b6d072f33e9
shahnawaz-pabon <s.pabon93@gmail.com>
2022-04-15 16:53:42 +0600

    bootstrap linked with AddTodo custom component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import "./styles/main.css";
+import AddTodo from "./components/AddTodo";
 
 export default function App() {
   const todos = useSelector((state) => state);
 
   return (
-    <div className="app">
+    <div className="todo-app">
       <h1>Todo List</h1>
+      <AddTodo />
     </div>
   );
 }
---
+++ src/components/AddTodo.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { v1 as uuid } from "uuid";
+
+import { addTodo } from "../redux/actions";
+
+const AddtTodo = () => {
+  const dispatch = useDispatch();
+  const [itemName, setItemName] = useState();
+
+  return (
+    <div className="my-3">
+      <input
+        className="from-control"
+        value={itemName}
+        onChange={(e) => setItemName(e.target.value)}
+      />
+      <button
+        className="btn btn-primary m-2"
+        onClick={() => {
+          dispatch(
+            addTodo({
+              id: uuid(),
+              item: itemName,
+            })
+          );
+          setItemName("");
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
+};
+
+export default AddtTodo;
